4e1536cc47c77a983b6356d4728e3dc02aa2093f
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2020-09-11 11:57:34 -0500

    extend clickable area of country cards

--- components/CountryCard/CountryCard.tsx
+++ components/CountryCard/CountryCard.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react'
+import React, {MouseEvent} from 'react'
 import Link from 'next/link'
 
 type CountryCardProps = {
@@ -6,31 +6,28 @@ type CountryCardProps = {
   image: string
 }
 
-const handleOnMouseOver = (event: MouseEvent)=> {
+const handleOnMouseOver = (event : MouseEvent) => {
   const element: HTMLElement = event.target as HTMLElement
   element.classList.add("opacity-75")
 }
 
-const handleOnMouseLeave = (event: MouseEvent)=> {
+const handleOnMouseLeave = (event : MouseEvent) => {
   const element: HTMLElement = event.target as HTMLElement
   element.classList.remove("opacity-75")
 }
 
-const CountryCard = ({name, image}: CountryCardProps) => {
-  return(
-    <>
-    <div className="main__country bg-white mx-2 my-2" >
-      <div className="h-48 w-48 text-white shadow-lg rounded-full flex flex-row justify-center font-bold items-center bg-cover bg-center transition duration-700 ease-in-out"
-        style={{ backgroundImage: `url(${image})` }}
-        onMouseOver={handleOnMouseOver}
-        onMouseLeave={handleOnMouseLeave}>
-        <Link href={"country/" + name}>
-          <a className="text-2xl text-center">{name}</a>
-        </Link>
-      </div>
-    </div>
-  </>
-  )
+const CountryCard = ({name, image} : CountryCardProps) => {
+  return (<div className="main__country bg-white mx-2 my-2">
+    <Link href={"country/" + name}>
+      <a>
+        <div className="h-48 w-48 text-white shadow-lg text-center text-2xl rounded-full flex flex-row justify-center font-bold items-center bg-cover bg-center transition duration-700 ease-in-out" style={{
+            backgroundImage: `url(${image})`
+          }} onMouseOver={handleOnMouseOver} onMouseLeave={handleOnMouseLeave}>
+          {name}
+        </div>
+      </a>
+    </Link>
+  </div>)
 }
 
 export default CountryCard
