f5a76a6e9234f6bb837954621a55086746d53d82
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2020-09-30 00:03:58 -0500

    Add Loading component

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+   <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-400 h-64 w-64"></div>
+  )
+}
+
+export default Loading
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import CountryCard from '@components/CountryCard/CountryCard'
 import Head from 'next/head'
+import Loading from '@components/Loading/Loading'
 
 type CountryCardProps = {
   id: string,
@@ -11,24 +12,36 @@ type CountryCardProps = {
 
 const Home = () => {
   const [countryList, setCountryList] = useState<TCountry[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
 
   useEffect(() => {
-    window.fetch('api/coffee').then(response => response.json()).then(({ data }: TAPICoffeeResponse) => {
+    window.fetch('api/coffee').then(response => {
+      setLoading(true)
+      return response.json()})
+      .then(({ data }: TAPICoffeeResponse) => {
+      setLoading(false)
+      setError("")
       setCountryList(data)
-    })
+    }).catch(error=>{
+      setError(error);
+      setLoading(false)})
   }, [])
   return (<div className="main flex flex-row flex-wrap justify-center content-start">
     <Head>
       <title>Home</title>
     </Head>
+
     <div className="text-2xl text-center w-full text-gray-700 py-8 font-bold"><h2>Know more about our coffee providers</h2></div>
     <div className="main__container flex flex-row flex-wrap justify-center items-center md:w-2/3">
 
-      {
+
+      {loading?<Loading/>:
         countryList.map(({ id, name, image }: CountryCardProps) => {
           return <CountryCard key={id} name={name} image={image} />
         })
       }
+      {error?<div className="text-red-400 font-bold">{error}</div>:null}
     </div>
   </div>)
 }
