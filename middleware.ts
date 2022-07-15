99ea18af0bea84e747140922e00b29c2d6a93e6e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-07-15 11:35:09 -0500

    Add dynamic routes to middleware auth protection

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,5 @@
 // This middleware is included in nextjs last version, redirects to login screen when non authenticated
 export { default } from "next-auth/middleware"
+// All urls contained in matcher will be processed by the middleware and will require authentication
+export const config = { matcher: ["/about", "/", "/country/:path*"] }
+
